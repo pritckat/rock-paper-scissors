5ea1fef023a691634468da9582b527d885d5e518
pritckat <pritckat@gmail.com>
2020-11-01 14:14:30 -0500

    add win and lose statement functions

--- rps.js
+++ rps.js
@@ -9,5 +9,17 @@ function playRound(playerSelection, computerSelection) {
     if (playerSelection === computerSelection) {
         return `It's a tie! Both players picked ${playerSelection}.`
     }
+    else if (playerSelection === 'rock') {
+        if (computerSelection === 'scissors') {
+
+        }
+    }
 }
 
+function winStatement(playerSelection, computerSelection) {
+    return `You win! ${playerSelection} beats ${computerSelection}.`
+}
+
+function loseStatement(playerSelection, computerSelection) {
+    return `You lose! ${computerSelection} beats ${playerSelection}.`
+}
